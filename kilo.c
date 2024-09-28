defc1606a17b080b246058f7afd1195733a1b5a3
Masayuki Keida <masayuki.keida+github@gmail.com>
2024-09-28 12:58:33 +0900

    5. A text editor; bug fix: disappearing the first line

--- kilo.c
+++ kilo.c
@@ -375,7 +375,7 @@ editorOpen(char *filename) {
 	char *line = NULL;
 	size_t linecap = 0;
 	ssize_t linelen;
-	linelen = getline(&line, &linecap, fp);
+
 	while ((linelen = getline(&line, &linecap, fp)) != -1) {
 		while (linelen > 0 && (line[linelen - 1] == '\n' ||
 				       line[linelen - 1] == '\r'))
